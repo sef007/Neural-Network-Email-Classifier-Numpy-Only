033e0fad2ba803c6d847dc3e3c8d5343a318a7f3
Dhru <118526140+sef007@users.noreply.github.com>
2023-07-19 14:54:01 +0100

    Update Optimised-Neural-Network-(V2).py

--- Optimised-Neural-Network-(V2).py
+++ Optimised-Neural-Network-(V2).py
@@ -7,6 +7,15 @@ from tqdm import tqdm
 
 class MyClassifier:
     def __init__(self, X_train, X_val, alpha, iterations):
+        """
+        Initialises the MyClassifier object.
+
+        Args:
+        X_train (ndarray): Training data.
+        X_val (ndarray): Validation data.
+        alpha (float): Learning rate.
+        iterations (int): Number of iterations for gradient descent.
+        """
         self.X_train = X_train
         self.X_val = X_val
         self.alpha = alpha
@@ -19,6 +28,15 @@ class MyClassifier:
         self.weights1, self.weights2, self.bias1, self.bias2 = self.initialize_parameters()
 
     def initialize_parameters(self):
+        """
+        Initialises the weights and biases for the neural network.
+
+        Returns:
+        weights1 (ndarray): Initial weights for the first layer.
+        weights2 (ndarray): Initial weights for the second layer.
+        bias1 (ndarray): Initial biases for the first layer.
+        bias2 (ndarray): Initial biases for the second layer.
+        """
         weights1 = np.random.randn(2, 55)
         weights2 = np.random.randn(2, 2)
         bias1 = np.random.randn(2, 1)
@@ -26,12 +44,41 @@ class MyClassifier:
         return weights1, weights2, bias1, bias2
 
     def rectified_linear_activation_funct(self, Z1):
+        """
+        Applies the rectified linear activation function to the input.
+
+        Args:
+        Z1 (ndarray): Input to the activation function.
+
+        Returns:
+        ndarray: Output after applying the activation function.
+        """
         return np.maximum(Z1, 0)
 
     def softmax_activation_funct(self, Z2):
+        """
+        applies the softmax activation function to the input Z
+
+        Args:
+            Z (numpy.ndarray): Input to the activation function
+
+        Returns:
+            numpy.ndarray: Result of applying the softmax activation function to Z
+        """
         return np.exp(Z2) / np.sum(np.exp(Z2), axis=0)
 
     def forward_propagation(self, weights1, weights2, bias1, bias2, X):
+        """
+        performs forward propagation through the neural network
+
+        args:
+            X (numpy.ndarray): Input data
+
+        returns:
+            tuple: Tuple containing the intermediate results of forward propagation
+                   (Z1, A1, Z2, A2)
+        """
+        
         Z1 = np.dot(weights1, X.T) + bias1
         layer1_out = self.rectified_linear_activation_funct(Z1)
 
@@ -41,6 +88,15 @@ class MyClassifier:
         return Z1, Z2, layer1_out, layer2_out
 
     def one_hot_encoding(self):
+        """
+        Converts the true labels to one-hot encoded form.
+
+        Args:
+            Y (numpy.ndarray): True labels.
+
+        Returns:
+            numpy.ndarray: One-hot encoded labels.
+        """
         Y = self.Y_train
         num_classes = np.max(Y) + 1
         encoded = np.zeros((num_classes, Y.size))
@@ -48,9 +104,35 @@ class MyClassifier:
         return encoded
 
     def ReLU_deriv(self, Z):
+        """
+        calculates the derivative of the rectified linear activation function
+
+        Args:
+        Z (ndarray): Input to the activation function.
+
+        Returns:
+        ndarray: Derivative of the activation function.
+        """
         return Z > 0
 
     def backward_propagation(self, Z1, Z2, layer1_out, layer2_out, weights2):
+        """
+        performs backward propagation through the neural network
+
+        Args:
+        Z1 (ndarray): Output of the first layer before activation.
+        Z2 (ndarray): Output of the second layer before activation.
+        layer1_out (ndarray): Output of the first layer after activation.
+        layer2_out (ndarray): Output of the second layer after activation.
+        weights2 (ndarray): Weights for the second layer.
+
+        Returns:
+        dW2 (ndarray): Gradient of weights2.
+        db2 (ndarray): Gradient of bias2.
+        dW1 (ndarray): Gradient of weights1.
+        db1 (ndarray): Gradient of bias1.
+        """
+
         one_hot_format = self.one_hot_encoding()
         m, n = self.dim
 
@@ -69,6 +151,9 @@ class MyClassifier:
         return dW2, db2, dW1, db1
 
     def update_parameters(self, weights1, bias1, weights2, bias2, dW1, db1, dW2, db2, alpha):
+        """
+        Updates the weights and biases using gradient descent.
+        """
         weights1 = weights1 - alpha * dW1
         bias1 = bias1 - alpha * db1
         weights2 = weights2 - alpha * dW2
@@ -76,14 +161,36 @@ class MyClassifier:
         return weights1, bias1, weights2, bias2
 
     def get_predictions(self, A2):
+        """
+        Returns the predictions based on the output of the neural network.
+
+        Args:
+        A2 (ndarray): Output of the second layer after activation.
+
+        Returns:
+        ndarray: Predicted class labels.
+        """
         return np.argmax(A2, axis=0)
         # Find the largest value in each col in matrix A2, this will be the prediction, the highest probability it is
         # in this class
 
     def get_accuracy(self, predictions, Y):
+        """
+        calculates the accuracy of the predictions.
+
+        Args:
+        predictions (ndarray): Predicted class labels
+        Y (ndarray): True class labels.
+
+        Returns:
+        float: Accuracy of the predictions.
+        """
         return np.sum(predictions == Y) / Y.size
 
     def gradient_descent(self):
+        """
+        performs gradient descent to train the neural network
+        """
         weights1, weights2, bias1, bias2 = self.initialize_parameters()
         for i in range(self.iterations):
             Z1, Z2, layer1_out, layer2_out = self.forward_propagation(weights1, weights2, bias1, bias2, self.X_train)
